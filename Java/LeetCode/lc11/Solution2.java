2dffb7ed756eedef384c7239c7f25d6cf6eac4a8
Ray <6518512@qq.com>
2022-12-31 00:43:18 +0800

    11

--- Java/LeetCode/lc11/Solution2.java
+++ Java/LeetCode/lc11/Solution2.java
@@ -4,19 +4,27 @@ package lc11;
  * 双指针：指针起点的多样性
  */
 public class Solution2 {
-    public int maxArea(int[] height) {
-        int left = 0, right = height.length-1;
-        int ret = (right - left) * Math.min(height[right], height[left]);
+   // public int maxArea(int[] height) {
+    //     int ret = 0;
+    //     int l=0,r=height.length-1;
+    //     while(l<r){
+    //         ret = Math.max((r-l)*Math.min(height[l],height[r]),ret);
+    //         if(height[l] < height[r]){
+    //             l++;
+    //         }else{
+    //             r--;
+    //         }
+    //     }
+    //     return ret;
+    // }
 
-        while (left < right) {
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
-            }
-            int tmp = (right - left) * Math.min(height[right], height[left]);
-            ret = Math.max(ret, tmp);
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length - 1, res = 0;
+        while(i < j) {
+            res = height[i] < height[j] ? 
+                Math.max(res, (j - i) * height[i++]): 
+                Math.max(res, (j - i) * height[j--]); 
         }
-        return ret;
+        return res;
     }
 }
